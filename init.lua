3d877d1fb14526a0d6709c1dc00bd91391da0521
Martin Eberlein <33397273+martineberlein@users.noreply.github.com>
2026-03-15 10:17:09 +0100

    fixed catppucin theme in neovim

--- init.lua
+++ init.lua
@@ -389,7 +389,11 @@ do
   -- Load the colorscheme here.
   -- Like many other themes, this one has different styles, and you could load
   -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-  vim.cmd.colorscheme 'tokyonight-night'
+  -- vim.cmd.colorscheme 'tokyonight-night'
+
+  vim.pack.add { { src = "https://github.com/catppuccin/nvim", name = "catppuccin" } }
+  vim.cmd.colorscheme "catppuccin-nvim"
+
 
   -- Highlight todo, notes, etc in comments
   vim.pack.add { gh 'folke/todo-comments.nvim' }
